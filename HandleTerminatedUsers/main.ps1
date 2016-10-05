3a9c5c62951e50bce5741231ae5ac55e52712082
David Berndtsson <david.berndtsson@gmail.com>
2016-10-05 16:54:10 +0200

    Update main.ps1
    Script to handle terminated users and homefolders.

--- HandleTerminatedUsers/main.ps1
+++ HandleTerminatedUsers/main.ps1
@@ -1,36 +1,74 @@
 $getuser = Get-aduser -filter * -SearchBase "OU=Slutat,OU=User,DC=david,DC=local" -Properties *
 $getDate = (Get-Date).AddDays(-30)
 
+$date = Get-Date -format "dd-MMM-yyyy"
+$logpath = "C:\Scripts\CleanUpTerminatedUsers\Logs\$date"+"Logfile.txt"
+
+
+#Function to log
+function saveLog($textToSave)
+{
+    $now = Get-Date -format "dd-MMM-yyyy HH:mm"
+    $textToSave = "`n" + $textToSave + " - $now"
+
+    $textToSave | Out-File $logpath -Encoding utf8 -Append
+}
+
 #Utility
 function deleteHomeFolder($folderToRemove){
-
-        New-Item -Path "C:\temp\Test\" -ItemType Directory
-        robocopy.exe "C:\Temp\Test" "$folderToRemove" /MIR
-        Remove-Item -Path "C:\Temp\Test" -Force -Recurse
+    try{
+        New-Item -Path "C:\temp\EmptyDummyFolder\" -ItemType Directory
+        robocopy.exe "C:\Temp\EmptyDummyFolder\" "$folderToRemove" /MIR
+        Remove-Item -Path "C:\Temp\EmptyDummyFolder\" -Force -Recurse
         Remove-Item -Path $folderToRemove -Force -Recurse
+        $logg = "$folderToRemove has been Removed."
+        saveLog($logg)
+        }
+    catch{
+    $logg = "ERROR: $folderToRemove hasnt been removed!"
+    saveLog($logg)
+    }
     
 }
 
 #Utility
 function renameFolder($folderToRename,$sam){
-    $oldName = Get-Item $folderToRename
-    $oldName = $oldName.Name
-    Rename-Item $folderToRename -NewName "$sam"
+    try{
+        $oldName = Get-Item $folderToRename
+        $oldName = $oldName.Name
+        Rename-Item $folderToRename -NewName "$sam"
+        $logg = "Old folder $folderToRename has changed name to $sam"
+        saveLog($logg)
+        }
+    catch{
+    $logg = "ERROR: $folderToRename hasnt changed name!"
+    }
 }
 
 #Utility
 function disableUser($userToDisable){
+
+try{
 Disable-ADAccount -Identity $userToDisable
+$logg = "ADUSer $userToDisable has been disabled."
     if($userToDisable -match "quit"){
         #Do nothing
         }
     else{
         Set-ADUser $userToDisable -SamAccountName "$userToDisable-quit"
+        $logg = "ADUser $usetToDisable has changed samaccountname to $userToDisable-quit"
+        saveLog($logg)
         }
+    }
+catch{
+$logg = "ERROR: ADUser $userToDisable hasnt been disabled and/or changed samaccoutname."
+}
 }
 
 #Main Function
 function handleUsers(){
+$logg = "Script started----------------"
+saveLog($logg)
     foreach($item in $getuser){
         $userWhenChange = $item.modifyTimeStamp
         $samaccountName = $item.SamAccountName
@@ -42,17 +80,17 @@ function handleUsers(){
             #>30days
 
             Remove-ADUser -Identity $samaccountName -Confirm:$false
-            #LOGGA
+            $logg = "ADUser $samaccountname has been removed."
+            saveLog($logg)
             deleteHomeFolder($homefolder2)
-            #LOGGA
 
             }
             else{
             #<30days
             disableUser($samaccountName)
-            #LOGGA
             renameFolder $homeFolder $samAccountname"_Quit"
-            #LOGGA
             }    
     }
+    $logg = "Script ended.`n------------------"
+    saveLog($logg)
 }
