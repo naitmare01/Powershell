b18cf6314e9ae4ae2c8f5aef3d2f5efbc6f0a739
David Berndtsson <david.berndtsson@gmail.com>
2016-10-06 19:02:51 +0200

    Update Test-PendingReboot.ps1
    Reworked the script.

--- Test-PendingReboot/Test-PendingReboot.ps1
+++ Test-PendingReboot/Test-PendingReboot.ps1
@@ -1,15 +1,45 @@
-function Test-PendingReboot
+#Based on <http://gallery.technet.microsoft.com/scriptcenter/Get-PendingReboot-Query-bdb79542>
+function Test-RebootRequired 
 {
- if (Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending" -EA Ignore) { return $true }
- if (Get-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired" -EA Ignore) { return $true }
- if (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name PendingFileRenameOperations -EA Ignore) { return $true }
- try { 
-   $util = [wmiclass]"\\.\root\ccm\clientsdk:CCM_ClientUtilities"
-   $status = $util.DetermineIfRebootPending()
-   if(($status -ne $null) -and $status.RebootPending){
-     return $true
-   }
- }catch{}
- 
- return $false
+    $result = @{
+        CBSRebootPending =$false
+        WindowsUpdateRebootRequired = $false
+        FileRenamePending = $false
+        SCCMRebootPending = $false
+    }
+
+    #Check CBS Registry
+    $key = Get-ChildItem "HKLM:Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending" -ErrorAction Ignore
+    if ($key -ne $null) 
+    {
+        $result.CBSRebootPending = $true
+    }
+   
+    #Check Windows Update
+    $key = Get-Item "HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired" -ErrorAction Ignore
+    if($key -ne $null) 
+    {
+        $result.WindowsUpdateRebootRequired = $true
+    }
+
+    #Check PendingFileRenameOperations
+    $prop = Get-ItemProperty "HKLM:SYSTEM\CurrentControlSet\Control\Session Manager" -Name PendingFileRenameOperations -ErrorAction Ignore
+    if($prop -ne $null) 
+    {
+        #PendingFileRenameOperations is not *must* to reboot?
+        #$result.FileRenamePending = $true
+    }
+    
+    #Check SCCM Client <http://gallery.technet.microsoft.com/scriptcenter/Get-PendingReboot-Query-bdb79542/view/Discussions#content>
+    try 
+    { 
+        $util = [wmiclass]"\\.\root\ccm\clientsdk:CCM_ClientUtilities"
+        $status = $util.DetermineIfRebootPending()
+        if(($status -ne $null) -and $status.RebootPending){
+            $result.SCCMRebootPending = $true
+        }
+    }catch{}
+
+    #Return Reboot required
+    return $result.ContainsValue($true)
 }
