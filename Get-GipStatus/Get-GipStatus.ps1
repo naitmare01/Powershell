b8790ca72886c97d8e56e7d41582f1193034edbf
David Berndtsson <david.berndtsson@gmail.com>
2017-03-28 10:51:52 +0200

    Create Get-GipStatus.ps1

---
+++ Get-GipStatus/Get-GipStatus.ps1
@@ -0,0 +1,35 @@
+function Get-GipStatus{
+param(
+[parameter(Mandatory=$true,ValueFromPipeline=$True)]
+[object]$User
+)
+    $distinguishedname = $User.DistinguishedName
+    #construct custom Object
+    $CustomObject = New-Object System.Object
+    
+    if($User.Memberof -match "G.Usr.*.GIP"){
+
+        $GIPStatus = "GIP"
+
+    }
+    else{
+
+        $GIPStatus = "EJ_GIP"
+
+    }
+
+    #Append user enhet and stift to the object
+    $enheten = $distinguishedname.Split(',')[2]
+    $enheten = $enheten -replace ("OU=","")
+
+    $stift = $distinguishedname.Split(',')[3]
+    $stift = $stift -replace("OU=","")
+
+    $CustomObject | Add-Member -Type NoteProperty -Name "GIP-Status" -Value $GIPStatus
+    $CustomObject | Add-Member -Type NoteProperty -Name "samaccountName" -Value $user.Samaccountname
+    $CustomObject | Add-Member -Type NoteProperty -Name "Enhet" -Value $enheten
+    $CustomObject | Add-Member -Type NoteProperty -Name "Stift" -Value $stift
+
+    # Return the value
+    return $CustomObject
+}
