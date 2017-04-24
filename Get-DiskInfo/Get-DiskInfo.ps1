fcfb47933acfdbe4d8c4b39df6a85d4134301b87
David Berndtsson <david.berndtsson@gmail.com>
2017-04-24 09:35:21 +0200

    Update Get-DiskInfo.ps1

--- Get-DiskInfo/Get-DiskInfo.ps1
+++ Get-DiskInfo/Get-DiskInfo.ps1
@@ -1,24 +1,28 @@
 function Get-DiskInfo{
 param(
-$Disk
+$Name
 )
-    $OutPutObject = New-Object System.Object
-    $UsedSpace_GB = ([Math]::Round($disk.Capacity /1GB,2)) - ([Math]::Round($disk.FreeSpace /1GB,2))
-    $OutPutObject | Add-Member -Type NoteProperty -Name "ServerName" -Value $disk.PSComputerName
-    $OutPutObject | Add-Member -Type NoteProperty -Name "UsedSpace_GB" -Value $UsedSpace_GB
-    $OutPutObject | Add-Member -Type NoteProperty -Name "Name" -Value $disk.Name
-    $OutPutObject | Add-Member -Type NoteProperty -Name "Label" -Value $disk.Label
-    return $OutPutObject
+    begin{
+        $DiskInfo = Get-WmiObject Win32_Volume -Filter "DriveType='3'" -ComputerName $Name
+    }
+
+    process{
+    $Target = @()
+        foreach($disk in $DiskInfo){
+            $OutPutObject = New-Object System.Object
+            $UsedSpace_GB = ([Math]::Round($disk.Capacity /1GB,2)) - ([Math]::Round($disk.FreeSpace /1GB,2))
+            $OutPutObject | Add-Member -Type NoteProperty -Name "ServerName" -Value $disk.PSComputerName
+            $OutPutObject | Add-Member -Type NoteProperty -Name "UsedSpace_GB" -Value $UsedSpace_GB
+            $OutPutObject | Add-Member -Type NoteProperty -Name "Name" -Value $disk.Name
+            $OutPutObject | Add-Member -Type NoteProperty -Name "Label" -Value $disk.Label
+            $Target += $OutPutObject
+        }
+    }
+
+    end{
+        return $Target
+    }
 }
 
 $Computers = "SERVER1", "SERVER2"
-
-foreach($computer in $computers){
-    $DiskInfo = Get-WmiObject Win32_Volume -Filter "DriveType='3'" -ComputerName $computer
-
-        foreach($disk in $DiskInfo){
-    
-            Get-DiskInfo -Disk $disk
-
-        }
-}
+Get-DiskInfo -Name $Computers
